0753e922a449de5d2c7efc2b2802bc666e91d154
novakvova <novakvova@gmail.com>
2020-02-22 14:46:08 +0200

    Add table and add view model add

---
+++ helloASP/Models/CategoryViewModels.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace helloASP.Models
+{
+
+    public class CategoryCreateViewModel
+    {
+        public virtual string Name { get; set; }
+
+        public virtual string UrlSlug { get; set; }
+
+
+        public virtual string Description { get; set; }
+    }
+}
